4b9d34d5113798e1986d975f2f50ba2f5a0832c3
Muhammad Amanah <73313340+chakiel404@users.noreply.github.com>
2025-04-05 01:43:37 +0700

    Add WebAPI/application/config/config.php 1743792216794

---
+++ WebAPI/application/config/config.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['base_url'] = 'http://localhost/your_project/';
+$config['index_page'] = '';
+$config['uri_protocol'] = 'REQUEST_URI';
+$config['url_suffix'] = '';
+$config['language'] = 'english';
+$config['charset'] = 'UTF-8';
+$config['enable_hooks'] = FALSE;
+$config['subclass_prefix'] = 'MY_';
+$config['composer_autoload'] = FALSE;
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['allow_get_array'] = TRUE;
+$config['cookie_secure'] = FALSE;
+$config['cookie_httponly'] = FALSE;
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+?>
